77057b6caba991678c5bb222a5d109ad705afa6c
takamura <yuhtakamura39@gmail.com>
2013-09-13 12:15:31 +0900

    grunt.js test

---
+++ Gruntfile.js
@@ -0,0 +1,8 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    cssmin: {},
+    watch: {}
+  });
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+};
